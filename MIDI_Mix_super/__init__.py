ad5831b05f23b44663283e63ad9ed450c0319be4
Rajmun Doman <domanr@gmail.com>
2024-03-16 11:04:43 +0100

    add MIDI Mix customised mapping

---
+++ MIDI_Mix_super/__init__.py
@@ -0,0 +1,10 @@
+import Live
+from .MIDI_Mix_super import MIDI_Mix_super
+
+
+def create_instance(c_instance):
+    """ Creates and returns the MIDI_Mix script """
+    return MIDI_Mix_super(c_instance)
+
+# local variables:
+# tab-width: 4
